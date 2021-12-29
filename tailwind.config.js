4b61648d455b33e977d04edbba53b9003af75fc9
Drabblesaur <tojohnny1559@gmail.com>
2021-12-29 00:15:57 -0800

    Initial Commit from create-next-app
    Initial commit with tailwind config setup. Also included basic brand colors.

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        colors:{
+            'background': '#FBF3E8',
+            'brand-blue': '#032830',
+            'brand-gray': '#7C6C77',
+            'brand-green': '#479F76',
+            'brand-orange': '#FD7E14',
+        },
+        fontFamily: {
+            'rubik-MonoOne': ['Rubik Mono One', 'sans-serif'],
+            'rubik': ['Rubik', 'sans-serif'],
+        },
+    },
+    plugins: [],
+  }
