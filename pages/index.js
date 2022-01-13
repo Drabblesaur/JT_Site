7c21b72041dd8263e233957e7c242498157c4f76
Drabblesaur <tojohnny1559@gmail.com>
2022-01-13 00:46:42 -0800

    fixed an error regarding naming

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Card from "../components/Card";
+import Card from "../components/card.js";
 export default function Home() {
   return (
     <div className="bg-back_svg  bg-contain bg-no-repeat bg-right">
